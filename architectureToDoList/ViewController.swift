cac5a8765c619181de4cd2d4ade85cddc92f44d7
Sofya Zakharova <sofyazakharova249@gmail.com>
2021-01-13 10:42:59 +0300

    Initial Commit

---
+++ architectureToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  architectureToDoList
+//
+//  Created by mac on 13.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
